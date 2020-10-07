ca72a1477d440152a4c878c9fb1f74c1fdabd228
lokeshreddy007 <lokeshreddy160@gmail.com>
2020-10-07 15:46:34 +0530

    Add Dashboard with Table Data and Snackbar

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,16 @@
                 Vuetify-Dash
             </v-toolbar-titile>
             <v-spacer></v-spacer>
-            <v-btn 
-            v-for="link in links"
-            :key="`${link.label}-header-link`"
-            text 
-            rounded  
-            :to="link.url">
-          {{link.label}}
-        </v-btn>
+            <v-btn v-for="link in links" :key="`${link.label}-header-link`" text rounded :to="link.url">
+                {{link.label}}
+            </v-btn>
         </v-app-bar>
         <v-content>
             <router-view></router-view>
         </v-content>
         <v-footer color="primary lighten-1" padless>
             <v-row justify="center" no-gutters>
-                <v-btn v-for="link in links" :key="`${link.label}-footer-link`" color="white" text rounded class="my-2"
-                :to="link.url">
+                <v-btn v-for="link in links" :key="`${link.label}-footer-link`" color="white" text rounded class="my-2" :to="link.url">
                     {{ link.label }}
                 </v-btn>
                 <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
@@ -39,15 +33,17 @@ export default {
     },
 
     data: () => ({
-        links:[
-        {
-          label:'Home',
-          url:'/'
-        },
-        {
-          label:'Login',
-          url:'/login'
-        }
+        links: [{
+                label: 'Home',
+                url: '/'
+            },
+            {
+                label: 'Login',
+                url: '/login'
+            }, {
+                label: 'Dashboard',
+                url: '/dashboard'
+            }
         ]
     }),
 };
